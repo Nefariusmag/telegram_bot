7cd2e079f4e8d9aaf63c68295c3743c5f28df143
Dima <nefariusmag@gmail.com>
2017-04-27 16:06:58 +0300

    Добавлен деплой ПОИБ, ПИЗИ

--- telegram_jenkins.py
+++ telegram_jenkins.py
@@ -36,7 +36,8 @@ def handle_start_help(message):
 Подсистема Пентахо: /gistek_pentaho \n
 Подсистема Портал: /gistek_portal  \n
 Подсистема мобильного приложения: /gistek_mobile \n
-Подсистема ПИЗИ: /gistek_pizi""")
+Подсистема ПИЗИ: /gistek_pizi \n
+Подсистема ПОИБ: /gistek_poib""")
 
 user_dict = {}
 
@@ -47,6 +48,7 @@ class Var:
         self.build_deloy = None
         self.issue_select = None
         self.issue_id = None
+        self.tag = None
 
 @bot.message_handler(commands=['gistek_build_arm'])
 def stend_select(message):
@@ -120,7 +122,7 @@ def action_select(message):
     global name_user
     name_user = "{}({}): ".format(message.chat.username, message.chat.id)
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    markup.add('Build', 'Deploy (not work yet)')
+    markup.add('Build', 'Deploy')
     msg = bot.reply_to(message, "Выберите, что будем делать", reply_markup=markup)
     bot.register_next_step_handler(msg, pentaho_app_select)
 
@@ -135,11 +137,66 @@ def pentaho_app_select(message):
             markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
             markup.add("FileProperties", "integr_clearcache", "integr_readmetadata", "languagePack", "pentaho-cas-tek", "Plugin", "quixote-theme")
             msg = bot.reply_to(message, "Выберите плагин для сборки:", reply_markup=markup)
-            bot.register_next_step_handler(msg, pentaho_job_jenkins)
+            bot.register_next_step_handler(msg, pentaho_build_job_jenkins)
+        if var.build_deloy == "Deploy":
+            markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+            markup.add('REA_TEST', 'PK', 'PI')
+            msg = bot.reply_to(message, "Выберите стенд", reply_markup=markup)
+            bot.register_next_step_handler(msg, pentaho_app_2_select)
     except Exception as e:
         bot.reply_to(message, 'oooops1')
 
+def pentaho_app_2_select(message):
+    try:
+        chat_id = message.chat.id
+        stend = message.text
+        var = user_dict[chat_id]
+        var.stend = stend
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add("update_plugins", "update_fileProperties", "update_quixote_theme", "update_langpack", "update_cas_tek", "update_mondrian")
+        msg = bot.reply_to(message, "Выберите что будем обновлять:", reply_markup=markup)
+        bot.register_next_step_handler(msg, pentaho_tag_select)
+    except Exception as e:
+        bot.reply_to(message, 'oooops2')
+
+def pentaho_tag_select(message):
+    try:
+        chat_id = message.chat.id
+        arm = message.text
+        var = user_dict[chat_id]
+        var.arm = arm
+        msg = bot.reply_to(message, "Введите номер версии (тег):")
+        bot.register_next_step_handler(msg, pentaho_job_jenkins)
+    except Exception as e:
+        bot.reply_to(message, 'oooops3')
+
 def pentaho_job_jenkins(message):
+    try:
+        chat_id = message.chat.id
+        tag = message.text
+        var = user_dict[chat_id]
+        var.tag = tag
+        bot.send_message(message.chat.id, "пыжимся и тужимся... ")
+        if var.arm == "update_plugins":
+            params = {"stend": var.stend, "tags": "update_plugins", "plugins_version": str(var.tag)}
+        if var.arm == "update_fileProperties":
+            params = {"stend": var.stend, "tags": "update_fileProperties", "fileProperties_version": str(var.tag)}
+        if var.arm == "update_quixote_theme":
+            params = {"stend": var.stend, "tags": "update_quixote_theme", "quixote_theme_version": str(var.tag)}
+        if var.arm == "update_langpack":
+            params = {"stend": var.stend, "tags": "update_langpack", "LanguagePacks_version": str(var.tag)}
+        if var.arm == "update_cas_tek":
+            params = {"stend": var.stend, "tags": "update_cas_tek", "cas_tek_version": str(var.tag)}
+        if var.arm == "update_mondrian":
+            params = {"stend": var.stend, "tags": "update_mondrian"}
+        print(name_user + "cтучится в jenkins чтобы выполнить " + str(var.arm) + " для ПОИБ")
+        jenkins.build_job('GISTEK_Pentaho/Update_Pentaho', params)
+        print(name_user + "на ПОИБ выполняется " + str(var.arm) + " версия " + str(var.tag))
+        bot.send_message(message.chat.id, "..еще 7 минут и приложение " + str(var.arm) + " на ПОИБ обновится, версия " + str(var.tag) + " (если ошибки в jenkins не будет), а пока можно продолжать..")
+    except Exception as e:
+        bot.reply_to(message, 'oooops4')
+
+def pentaho_build_job_jenkins(message):
     try:
         chat_id = message.chat.id
         arm = message.text
@@ -329,5 +386,74 @@ def pizi_job_jenkins(message):
             bot.send_message(message.chat.id, "..еще 5 минуточек и приложение " + str(var.arm) + " на Сборе обновится (если ошибки в jenkins не будет), а пока можно продолжать..")
     except Exception as e:
         bot.reply_to(message, 'oooops3')
+
+@bot.message_handler(commands=['gistek_poib'])
+def action_select(message):
+    global name_user
+    name_user = "{}({}): ".format(message.chat.username, message.chat.id)
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.add('Build', 'Deploy')
+    msg = bot.reply_to(message, "Выберите, что будем делать", reply_markup=markup)
+    bot.register_next_step_handler(msg, poib_select)
+
+def poib_select(message):
+    try:
+        chat_id = message.chat.id
+        build_deloy = message.text
+        var = Var(build_deloy)
+        user_dict[chat_id] = var
+        var.build_deloy = build_deloy
+        if var.build_deloy == "Build":
+            bot.send_message(message.chat.id, "пыжимся и тужимся... ")
+            print(name_user + "cтучится в jenkins чтобы собрать приложение для ПОИБ")
+            jenkins.build_job('GISTEK_Poib/Build')
+            print(name_user + "собирает ПОИБ")
+            bot.send_message(message.chat.id, "..еще 5 минуточек и ПОИБ соберется (если ошибки в jenkins не будет), а пока можно продолжать..")
+        if var.build_deloy == "Deploy":
+            markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+            markup.add('REA_TEST', 'PK', 'PI')
+            msg = bot.reply_to(message, "Выберите стенд", reply_markup=markup)
+            bot.register_next_step_handler(msg, poib_app_select)
+    except Exception as e:
+        bot.reply_to(message, 'oooops1')
+
+def poib_app_select(message):
+    try:
+        chat_id = message.chat.id
+        stend = message.text
+        var = user_dict[chat_id]
+        var.stend = stend
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add("Update_DB", "Update_App")
+        msg = bot.reply_to(message, "Выберите что будем обновлять:", reply_markup=markup)
+        bot.register_next_step_handler(msg, poib_tag_select)
+    except Exception as e:
+        bot.reply_to(message, 'oooops2')
+
+def poib_tag_select(message):
+    try:
+        chat_id = message.chat.id
+        arm = message.text
+        var = user_dict[chat_id]
+        var.arm = arm
+        msg = bot.reply_to(message, "Введите номер версии (тег):")
+        bot.register_next_step_handler(msg, poib_job_jenkins)
+    except Exception as e:
+        bot.reply_to(message, 'oooops3')
+
+def poib_job_jenkins(message):
+    try:
+        chat_id = message.chat.id
+        tag = message.text
+        var = user_dict[chat_id]
+        var.tag = tag
+        bot.send_message(message.chat.id, "пыжимся и тужимся... ")
+        params = {"stend": var.stend, "version": str(var.tag)}
+        print(name_user + "cтучится в jenkins чтобы выполнить " + str(var.arm) + " для ПОИБ")
+        jenkins.build_job('GISTEK_Poib/' + str(var.arm), params)
+        print(name_user + "на ПОИБ выполняется " + str(var.arm) + " версия " + str(var.tag))
+        bot.send_message(message.chat.id, "..еще 2 минуты и приложение " + str(var.arm) + " на ПОИБ обновится, версия " + str(var.tag) + " (если ошибки в jenkins не будет), а пока можно продолжать..")
+    except Exception as e:
+        bot.reply_to(message, 'oooops4')
 
 bot.polling()
