2cbd813c75d83717b30d907a143586e70ddf0fb3
Dima Erokhin <i9164871362@gmail.com>
2017-06-19 08:39:59 +0300

    Добавил Смолина

--- config.py
+++ config.py
@@ -3,4 +3,4 @@
 token = '357145828:AAEKuo0euMlBGQvUZNLiicj5823mMxPoTuQ'
 username = "jenkins-service"
 password = "jenkins-service123jenkins-service"
-true_id = [121860960, 26018251]
+true_id = [121860960, 190766771, 26018251]
