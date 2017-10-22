866dbc0ea15dde3014038994aa7ef0ad0fbad652
Dima <nefariusmag@gmail.com>
2017-10-22 22:00:01 +0300

    Сменил положение в конфиге

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 
 url-jenkins = "http://jenkins.ru"
 url-jenkins2 = "http://jenkins-2.ru"
-token = '<token-telegram>'
-token_gitlab = 'token-gitlab'
 username = "user-jenkins"
 password = "pass-jenkins"
+token = '<token-telegram>'
+url_gitlab = "http://gitlab"
+token_gitlab = 'token-gitlab'
 true_id = [n1, n2, n3]
 true_id_dev = [n1, n2, n3, n4, n5, n6, n7]
-url_gitlab = "http://gitlab"
